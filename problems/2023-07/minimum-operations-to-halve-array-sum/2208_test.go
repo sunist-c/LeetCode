dd4edd800811a14c4caeba2d5d69676b78a80559
sunist-c <sunist@mail.sunist.cn>
2023-07-25 10:40:30 +0800

    ADD: 添加了2023.7.25-MinimumOperationsToHalveArraySum的刷题记录

---
+++ problems/2023-07/minimum-operations-to-halve-array-sum/2208_test.go
@@ -0,0 +1,25 @@
+package minimum_operations_to_halve_array_sum
+
+import (
+	"leetcode/cases"
+	"testing"
+)
+
+func TestMinimumOperationsToHalveArraySum(t *testing.T) {
+	testCases := []cases.CommonJudgeTestCase[[]int, int]{
+		{
+			Input:      []int{5, 19, 8, 1},
+			WantOutput: 3,
+		},
+		{
+			Input:      []int{1000, 1000, 3, 7},
+			WantOutput: 1,
+		},
+		{
+			Input:      []int{3, 8, 20},
+			WantOutput: 3,
+		},
+	}
+
+	cases.NewCommonTestCases("MinimumOperationsToHalveArraySum", halveArray, testCases...).Run(t)
+}
